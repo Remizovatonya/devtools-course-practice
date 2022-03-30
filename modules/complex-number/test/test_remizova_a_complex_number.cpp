ec938a5310f84b7994d116d06926568316ac4e87
Remizova Antonina <Rearo5g@yandex.ru>
2022-03-30 22:35:26 +0300

    Updated tests

--- modules/complex-number/test/test_remizova_a_complex_number.cpp
+++ modules/complex-number/test/test_remizova_a_complex_number.cpp
@@ -5,7 +5,6 @@
 #include "include/complex_number.h"
 
 TEST(Remizova_Antonina_ComplexNumberTest, Can_Create_ComplexNumber) {
-
     ComplexNumber a(5.0, 3.0);
 
     EXPECT_EQ(5.0, a.getRe());
@@ -13,7 +12,6 @@ TEST(Remizova_Antonina_ComplexNumberTest, Can_Create_ComplexNumber) {
 }
 
 TEST(Remizova_Antonina_ComplexNumberTest, Can_Create_Copy_ComplexNumber) {
-
     ComplexNumber a(3.0, 0.1);
     ComplexNumber b(a);
 
@@ -22,7 +20,6 @@ TEST(Remizova_Antonina_ComplexNumberTest, Can_Create_Copy_ComplexNumber) {
 }
 
 TEST(Remizova_Antonina_ComplexNumberTest, Can_Assign_ComplexNumber) {
-
     ComplexNumber a(3.0, 0.8);
     ComplexNumber b(4.0, 0.9);
 
@@ -33,7 +30,6 @@ TEST(Remizova_Antonina_ComplexNumberTest, Can_Assign_ComplexNumber) {
 }
 
 TEST(Remizova_Antonina_ComplexNumberTest, Can_Summarize_ComplexNumber) {
-
     ComplexNumber a(4.0, 0.5);
     ComplexNumber b(2.0, 0.6);
     ComplexNumber sum = a + b;
@@ -43,7 +39,6 @@ TEST(Remizova_Antonina_ComplexNumberTest, Can_Summarize_ComplexNumber) {
 }
 
 TEST(Remizova_Antonina_ComplexNumberTest, Can_Subtract_ComplexNumber) {
-
     ComplexNumber a(3.0, 2.0);
     ComplexNumber b(5.0, 0.7);
     ComplexNumber dif = a - b;
@@ -53,7 +48,6 @@ TEST(Remizova_Antonina_ComplexNumberTest, Can_Subtract_ComplexNumber) {
 }
 
 TEST(Remizova_Antonina_ComplexNumberTest, Can_Multiply_ComplexNumber) {
-
     ComplexNumber a(1.0, 1.0);
     ComplexNumber b(4.0, 2.0);
     ComplexNumber multi = a * b;
